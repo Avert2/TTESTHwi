d8f664fcc655c18c9dbed4227fe94caa1a09d0fe
Erwin <blizzardfire1412@gmail.com>
2025-08-21 16:02:05 +0700

    add 1 and 3

---
+++ 1_Backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const submissions  = [];
+app.post('/submission', (req, res)=> {
+    const {name,email,phone } = req.body;
+    if (!name || (!email && !phone)){
+        return res.status(400).json({error: 'Name and Email/Phone Number is required'});
+    }
+    const id = crypto.randomUUID();
+    submissions.push({id, name, email,phone });
+    res.status(201).json({message: 'You have been registered', data: {id, name, email,phone}});
+});
+
+
+app.get('/submissions', (req, res) => {
+  res.json(submissions);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+app.use(express.static('public'));
+
